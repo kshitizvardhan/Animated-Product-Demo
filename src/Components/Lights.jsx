fdd150015dd7fb9018de1cffbb75827398e7b407
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-24 03:37:14 +0530

    Add ModelView Component

--- src/Components/Lights.jsx
+++ src/Components/Lights.jsx
@@ -4,16 +4,10 @@ const Lights = () => {
   return (
     // group different lights and lightformers. We can use group to organize lights, cameras, meshes, and other objects in the scene.
     <group name="lights">
-      {/**
-       * @description Environment is used to create a background environment for the scene
-       * https://github.com/pmndrs/drei?tab=readme-ov-file#environment
-       */}
+      {/* Environment is used to create a background environment for the scene */}
       <Environment resolution={256}>
         <group>
-          {/**
-           * @description Lightformer used to create custom lights with various shapes and properties in a 3D scene.
-           * https://github.com/pmndrs/drei?tab=readme-ov-file#lightformer
-           */}
+          {/* Lightformer used to create custom lights with various shapes and properties in a 3D scene. */}
           <Lightformer
             form="rect"
             intensity={10}
@@ -38,11 +32,8 @@ const Lights = () => {
         </group>
       </Environment>
 
-      {/**
-       * @description spotLight is used to create a light source positioned at a specific point
-       * in the scene that emits light in a specific direction.
-       * https://threejs.org/docs/#api/en/lights/SpotLight
-       */}
+      {/* spotLight is used to create a light source positioned at a specific point
+       * in the scene that emits light in a specific direction. */}
       <spotLight
         position={[-2, 10, 5]}
         angle={0.15}
