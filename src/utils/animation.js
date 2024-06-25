7d1efce80bed5364ec698188df707c370a243c5a
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-26 01:46:27 +0530

    Add helper function for GSAP animations with scroll trigger

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,3 +1,19 @@
+import gsap from "gsap";
+
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+    gsap.to(target, {
+        ...animationProps,
+        scrollTrigger: {
+            trigger: target,
+            toggleActions: "restart reverse restart reverse",
+            start: "top 85%",
+            ...scrollProps,
+        }
+    })
+};
+
+
+
 export const animationWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
     timeline.to(rotationRef.current.rotation, {
         y: rotationState,
@@ -16,3 +32,13 @@ export const animationWithGsapTimeline = (timeline, rotationRef, rotationState, 
         ease:"power2.inOut"
     }, "<")
 }
+
+/*
+
+toggleActions: Determines how the linked animation is controlled at the 4 distinct toggle places - onEnter, onLeave, onEnterBack, and onLeaveBack, in that order. 
+The default is play none none none. 
+So toggleActions: "play pause resume reset" will play the animation when entering, pause it when leaving, resume it when entering again backwards, and reset (rewind back to the beginning) when scrolling all the way back past the beginning. 
+
+We can use any of the following keywords for each action: "play", "pause", "resume", "reset", "restart", "complete", "reverse", and "none".
+
+*/
