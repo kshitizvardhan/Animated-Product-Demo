2bad1f03b533ed10bbb56e9d574ca6cdbe8dad2a
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-26 03:36:21 +0530

    refactored animations

--- src/Components/Highlights.jsx
+++ src/Components/Highlights.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import gsap from 'gsap'
 import { rightImg, watchImg } from '../utils'
 import VideoCarousel from './VideoCarousel'
+import { animateWithGsap } from '../utils/animation'
 
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to("#title",{
-      opacity: 1,
-      y: 0
-    })
-    gsap.to(".link",{ opacity:1, y:0, duration: 1, stagger: 0.25})
+    animateWithGsap("#title",
+      { opacity: 1, y: 0 }
+    )
+    animateWithGsap(".link",{ opacity:1, y:0, duration: 0.9, stagger: 0.25})
   }, [])
 
   return (
